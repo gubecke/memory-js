11daf0498e534264af1072e96ba4b87f26ec0057
Guillaume Becker <guillaume.becker2013@gmail.com>
2024-04-26 10:29:51 +0200

    console.log card Ids

--- app.js
+++ app.js
@@ -50,13 +50,21 @@ const cardArray = [
 ]
 cardArray.sort(() => 0.5 - Math.random())
 const gridDisplay = document.querySelector('#grid')
-console.log(gridDisplay)
+const cardsChosen = []
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
     }
 }
 createBoard()
+function flipCard() {
+    console.log(cardArray)
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    console.log('clicked', cardId)
+    console.log(cardsChosen)
+}
